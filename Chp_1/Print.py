50cf144a0775168f433f0128bdc15f6441f1adf8
Noor ul haq <47422949+noorhaq@users.noreply.github.com>
2020-06-01 07:24:55 +0500

    Update Print.py

--- Chp_1/Print.py
+++ Chp_1/Print.py
@@ -2,4 +2,7 @@
 print(a); #Printing it
 a = "123";#Redefing a as string
 print("Hello ", a[0], a[1], a[2]);#Printing a by parts one memory/character at a time
-print(a); #Prinitng a as a whole
+print("_________"); #Prinitng 
+print("Hello ", a[0], a[1], a[2],sep=' ');#For changing seperation between printing variables
+print("Hello ",end=''); #For removing end of line case
+print('World');
