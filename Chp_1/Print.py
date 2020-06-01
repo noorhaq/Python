2c598cb99c166bf12ee3c2cbc59c9206580abb3a
Noor ul haq <47422949+noorhaq@users.noreply.github.com>
2020-06-01 06:50:30 +0500

    Create Print.py

---
+++ Chp_1/Print.py
@@ -0,0 +1,5 @@
+ a = 30;  #Initializing int variable with value 30
+print(a); #Printing it
+a = "123";#Redefing a as string
+print("Hello ", a[0], a[1], a[2]);#Printing a by parts one memory/character at a time
+print(a); #Prinitng a as a whole
